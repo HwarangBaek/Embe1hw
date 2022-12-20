78b2c0c98167f498a580cd4ab44cf7aaa0481112
Hwarang Baek <bcpkbcpk@naver.com>
2022-12-20 17:11:04 +0900

    막써봄

--- 1_makSSm.c
+++ 1_makSSm.c
@@ -18,654 +18,184 @@ df
 asd
 fas
 dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
+
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
+sdf
+asdfa
 df
-asdfas
+asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
+fas
+dfas
+df#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>   //헤더파일
 
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
+int _init init_hello(void){ //프로그램 시작을 알림
+    printk("Hello world.\n");
+    return 0;
+}
+void _exit exit_hello(void){
+    printk("Good bye\n");
+}
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
 sdf
+asdfa
+df
 asd
-f
-ds
-f
-sad
-fasd
-f
-asd
-
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
-sdf
-asd
-f
-ds
-f
-sad
-fasd
-f
-asd
-
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-fdf
-asdfas
-
-dfasdfsad
-fsad
-fsda
-f
-sadf
-sdf
-asd
-f
-ds
-f
-sad
-fasd
-f
-asd
-
-sd
-f
-asr
-
-adfh
-ad
-h
-dg
-hdsf
-h
-sdgh
-adf
-hfa
-gh
-
-g
-sd
-gsd
-fg
-sdfg
-dsh
-
-dsh
-dasf
-g
-awr
-
-vs
-b
-
-f
-vbseg
-ns
-
-aegf
-hawf
-ba
-bnsdfg
-nsf
-nfh
-mh
-m
-nsg
-b
-sdgb
-sgdb
-fgn
-f
+fas
+dfas
+df
